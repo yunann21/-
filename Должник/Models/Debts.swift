aafc0b728195409ae5e5acd016b2427b0e65ee76
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-02-17 20:46:15 +0300

    add direction to debt model

--- Должник/Models/Debts.swift
+++ Должник/Models/Debts.swift
@@ -11,14 +11,16 @@ import Foundation
 final class Debts: Object {
     @Persisted var personName: String
     @Persisted var debtSize: Int
+    @Persisted var direction: Bool
     @Persisted var startDate: Date
     @Persisted var finishDate: Date
     @Persisted var comment: String
     
-    convenience init(personName: String, debtSize: Int, startDate: Date, finishDate: Date, comment: String = "") {
+    convenience init(personName: String, debtSize: Int, direction: Bool, startDate: Date, finishDate: Date, comment: String = "") {
         self.init()
         self.personName = personName
         self.debtSize = debtSize
+        self.direction = direction
         self.startDate = startDate
         self.finishDate = finishDate
         self.comment = comment
