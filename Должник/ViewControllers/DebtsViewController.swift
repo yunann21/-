9b5671b1451fb177437f8b07c71f5a5fd1c62045
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-02-17 21:24:05 +0300

    did delegate

--- Должник/ViewControllers/DebtsViewController.swift
+++ Должник/ViewControllers/DebtsViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 import RealmSwift
 
+protocol NewDebtViewControllerDelegate: AnyObject {
+    func addDebt(
+        personName: String,
+        debtSize: Int,
+        direction: Bool,
+        startDate: Date,
+        finishDate: Date,
+        comment: String
+    )
+}
+
 final class DebtsViewController: UITableViewController {
     private let storageManager = StorageManager.shared
     private var debts: Results<Debts>!
@@ -16,9 +27,34 @@ final class DebtsViewController: UITableViewController {
         super.viewDidLoad()
         debts = storageManager.fetchData(Debts.self)
         storageManager.save()
-        
     }
-
-
 }
 
+// MARK: - UITableViewDataSource
+extension DebtsViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        debts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Debt", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        let debt = debts[indexPath.row]
+        
+        content.text = debt.personName
+        content.secondaryText = debt.debtSize.description
+        content.secondaryTextProperties.color = debt.direction ? .green : .red
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
+
+// MARK: - NewDebtViewControllerDelegate
+extension DebtsViewController: NewDebtViewControllerDelegate {
+    func addDebt(personName: String, debtSize: Int, direction: Bool, startDate: Date, finishDate: Date, comment: String) {
+        print("add debt")
+    }
+}
